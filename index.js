f638f96a7d7819996af52a8c195145afebd6df32
Danielbyte <danielmasango7@gmail.com>
2021-06-16 23:28:16 +0200

    port fixed

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/createaccount', accountRouter)
 app.use('/cdn', express.static('public'))
 
-app.listen(3000)
+const port = process.env.PORT || 3000
 
-console.log('listening on port 3000')
+app.listen(port)
+
+console.log('listening on port 3000', port)
